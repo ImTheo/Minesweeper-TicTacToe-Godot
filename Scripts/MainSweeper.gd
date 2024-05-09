413458cc7e6f6e6079b6bbe5c170eef88af795f7
theoSisa <153256596+theoSisa@users.noreply.github.com>
2024-05-09 00:29:19 -0500

    finished minesweeper

--- Scripts/MainSweeper.gd
+++ Scripts/MainSweeper.gd
@@ -2,32 +2,96 @@ extends Control
 
 @onready var label_mines_left = %Label_minesLeft
 @onready var label_mine = %Label_mine
-var squares:Array[Square]
-func _ready():
-	for node in get_tree().get_nodes_in_group("squares"):
-		squares.append(node)
-	for i in squares:
-		i.connect("game_ended_signal", end_game)
-	plant_mines(squares)
-		
+@onready var grid_container_minesweeper = %GridContainer_Minesweeper
+var board_squares:Array[Square]
+const SQUARE_NUMBER = 100
+const MINES = 10
+const SQUARE = preload("res://Scenes/Square.tscn")
+var board_array = []
 
-func plant_mines(squares:Array[Square]):
-	var mines = get_unique_random_numbers()
-	for i in mines:
-		squares[i].mine_square()
-		
-	label_mines_left.text = str(mines.size())
+func _ready():
+	fill_board_array()
+	create_board()
+	plant_mines()
+	generate_hints()
+	#for i in get_tree().get_nodes_in_group("hints"):
+		#print(i)
+
+func fill_board_array():
+	var row:Array[int] = []
+	var matrix_rows:int = SQUARE_NUMBER/grid_container_minesweeper.columns
+	var matrix_columns:int = grid_container_minesweeper.columns
+	for i in range(matrix_rows):
+		for j in range(matrix_columns):
+			row.append(matrix_columns*i+j)
+		board_array.append(row)
+		row = []
+
+func reveal_empty_squares(square:Square):
+	if square.is_square_pressed() or square.is_mined():
+		return
+	square.reveal_tile()
+	var adyacents:Array = square.get_adyacent_elements(board_array)
+	for i in adyacents:
+		if board_squares[i].hint_score != 0:
+			board_squares[i].reveal_tile()
+		if board_squares[i].hint_score == 0:
+			reveal_empty_squares(board_squares[i])
+
+func check_win():
+	for i in get_tree().get_nodes_in_group("hints"):
+		if not i.is_square_pressed():
+			print(i.index)
+			print(i.index_to_coordinates())
+			print("No has ganado")
+			return
+	print("Ganaste")
+	end_game("Ganaste")
+
+func create_board():
+	var square:Square
+	for i in range(SQUARE_NUMBER):
+		square = SQUARE.instantiate()
+		square.connect("game_ended_signal", end_game)
+		square.connect("hint_square_pressed_signal", check_win)
+		square.connect("empty_square_pressed_signal", reveal_empty_squares)
+		square.connect("flagged_mine_signal", update_flag)
+		square.index = i
+		board_squares.append(square)
+		grid_container_minesweeper.add_child(square)
+
+func update_flag(is_added):
+	var mines_left:int = int(label_mines_left.text)
+	if is_added:
+		mines_left -= 1
+	else:
+		mines_left += 1
+	label_mines_left.text = str(mines_left)
+
+func generate_hints():
+	var adyacents:Array
+	for i in get_tree().get_nodes_in_group("mines"):
+		adyacents = i.get_adyacent_elements(board_array)
+		for adyacent_index in adyacents:
+			board_squares[adyacent_index].add_hint()
+
+func plant_mines():
+	var mines_indexes:Array[int] = get_unique_random_numbers()
+	for i:int in mines_indexes:
+		board_squares[i].mine_square()
+			
+	label_mines_left.text = str(mines_indexes.size())
 
 func get_unique_random_numbers()->Array[int]:
-	var numbers:Array[int]
-	while numbers.size() < 10:
-		var number = randi() % 100
+	var numbers:Array[int] = []
+	while numbers.size() < MINES:
+		var number = randi() % SQUARE_NUMBER
 		if not numbers.has(number):
 			numbers.append(number)
 	return numbers
 
-func end_game():
-	label_mine.text = "Perdiste"
+func end_game(message:String):
+	label_mine.text = message
 	label_mines_left.text = ""
 	
 	for i:Square in get_tree().get_nodes_in_group("squares"):
--- Scripts/Square.gd
+++ Scripts/Square.gd
@@ -10,45 +10,78 @@ const TYPE_3 = preload("res://assets/type3.svg")
 const FLAG = preload("res://assets/flag.svg")
 const MINE = preload("res://assets/mine.svg")
 const MINE_RED = preload("res://assets/mine_red.svg")
+const TYPE_4 = preload("res://assets/type4.svg")
+const TYPE_5 = preload("res://assets/type5.svg")
+const TYPE_6 = preload("res://assets/type6.svg")
+const TYPE_7 = preload("res://assets/type7.svg")
+const TYPE_8 = preload("res://assets/type8.svg")
 #endregion
 
 @onready var square = self
-var pressed_cell:bool = false
+var pressed_square:bool = false
 var flagged:bool = false
 var mined:bool = false
 var index
+var hint_score = 0
 
-signal square_pressed_signal(index:int,node:Square)
+signal empty_square_pressed_signal(node:Square)
 signal game_ended_signal()
+signal flagged_mine_signal()
+signal hint_square_pressed_signal()
+
+func is_mined():
+	return mined
+
+func is_square_pressed():
+	return pressed_square
 
 func _ready():
-	pass
-	#if randi() % 11 == 0:
-		#mined = true
-		#set_texture_square(MINE)
-		#add_to_group("mines")
-	#else:
-		#set_texture_square(CLOSED)
-
-func cell_not_pressed():
-	if not pressed_cell:
-		pressed_cell = true
-		return true
+	set_texture_square(CLOSED)
 
 func update_tile():
-	if not pressed_cell and not mined:
-		pressed_cell = !pressed_cell
-		set_texture_square(TYPE_0)
+	if hint_score == 0:
+		empty_square_pressed_signal.emit(self)
+	if not pressed_square and not mined and not flagged:
+		pressed_square = true
+		hint_square_pressed_signal.emit()
+		reveal_tile()
 	if mined and not flagged:
-		game_ended_signal.emit()
+		game_ended_signal.emit("Perdiste")
+
+func reveal_tile():
+	pressed_square = true
+	if mined:
+		set_texture_square(CLOSED)
+	if hint_score == 0:
+		set_texture_square(TYPE_0)
+	elif hint_score == 1:
+		set_texture_square(TYPE_1)
+	elif hint_score == 2:
+		set_texture_square(TYPE_2)
+	elif hint_score == 3:
+		set_texture_square(TYPE_3)
+	elif hint_score == 4:
+		set_texture_square(TYPE_4)
+	elif hint_score == 5:
+		set_texture_square(TYPE_5)
+	elif hint_score == 6:
+		set_texture_square(TYPE_6)
+	elif hint_score == 7:
+		set_texture_square(TYPE_7)
+	elif hint_score == 8:
+		set_texture_square(TYPE_8)
 
 func mark_flag_square():
-	if not pressed_cell and not flagged:
+	if pressed_square:
+		return
+	if not pressed_square and not flagged:
 		set_texture_square(FLAG)
 		flagged = true
+		flagged_mine_signal.emit(true)
 		return
 	set_texture_square(CLOSED)
 	flagged = false
+	flagged_mine_signal.emit(false)
 
 func set_texture_square(texture):
 	square.texture_normal = texture
@@ -60,13 +93,41 @@ func _on_gui_input(event):
 				update_tile()
 			MOUSE_BUTTON_RIGHT:
 				mark_flag_square()
-			MOUSE_BUTTON_MIDDLE:
-				print("middle")
+
+func index_to_coordinates() -> Vector2:
+	var num_columns: int = 10
+	var row: int = index / num_columns
+	var column: int = index % num_columns
+	return Vector2(row, column)
 
 func mine_square():
 	mined = true
-	set_texture_square(MINE)
+	#set_texture_square(MINE)
 	add_to_group("mines")
+	
+func is_valid_pos(i, j, n, m):
+	return i >= 0 and j >= 0 and i < n and j < m
+
+func get_adjacent(arr,pos)->Array:
+	var n = len(arr)
+	var m = len(arr[0])
+	var v = []
+	for dx in range(-1 if pos.x > 0 else 0, 2 if pos.x < n - 1 else 1):
+		for dy in range(-1 if pos.y > 0 else 0, 2 if pos.y < m - 1 else 1):
+			if dx != 0 or dy != 0:
+				v.append(arr[pos.x + dx][pos.y + dy])
+	return v
+
+func get_adyacent_elements(board_array)->Array:
+	var index_position:Vector2 = index_to_coordinates()
+	var adyacent_list:Array = get_adjacent(board_array,index_position)
+	return adyacent_list
+
+func add_hint():
+	if not mined:
+		hint_score = hint_score + 1
+		if not is_in_group("hints"):
+			add_to_group("hints")
 
 func disable_square():
 	square.disabled = true
